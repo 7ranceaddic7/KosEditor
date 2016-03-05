df815c54680208084f28e934e15404b6fe67414e
Tatjam <tajampi@gmail.com>
2016-03-05 23:48:04 +0100

    A simple 4 bit pallete color class

---
+++ KosEditor/Display.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KosEditor
+{
+
+    // This class handles display of the 
+    // generated bitmap and the overlay
+    // "retro-style" image
+    // Relies on Unity UI
+    //-------------------------------------
+    // Interface between other classes and Unity
+
+    public class Window
+    {
+
+    }
+}
